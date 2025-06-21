e19e04e78369cc029bc2a36c8f313323dcd664f7
c1c3ru <cicerosilva.ifce@gmail.com>
2025-06-21 15:08:21 -0300

    Revert "feat: Integrate dotenv for environment variable management and update dependencies"
    This reverts commit fbeddb8f9d70eed02fb5c0fb0abe4f0f3a2e9c6d.

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app_module.dart';
 import 'app_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Load .env file
-  await dotenv.load(fileName: ".env");
-
   // Initialize SharedPreferences
   await SharedPreferences.getInstance();
 
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    url: 'https://rfqoyhebyozwtqwlrhyx.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmcW95aGVieW96d3Rxd2xyaHl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NDAwMjMsImV4cCI6MjA2NDAxNjAyM30.7GXf3tVYUzj1nJnUa--e0DYR5pUd__ifbilL-_vPnk4',
   );
 
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
